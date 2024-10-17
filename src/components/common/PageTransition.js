249e8bf104b1c741c7319e3853f6a7a746382436
Nicoll Douglas <nicoll.leo.douglas@gmail.com>
2024-10-17 14:40:16 +0100

    file/refactor: move transition props

--- src/components/common/PageTransition.js
+++ src/components/common/PageTransition.js
@@ -1,8 +1,16 @@
 "use client";
 
 import { motion } from "framer-motion";
-import * as tr from "@/constants/transition";
 
-export default function PageTransition({ variant = "opacity", children }) {
-  return <motion.div {...tr[variant]}>{children}</motion.div>;
+export default function PageTransition({ children }) {
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 1, delay: 0.2 }}
+    >
+      {children}
+    </motion.div>
+  );
 }
--- src/constants/transition.js
+++
@@ -1,8 +0,0 @@
-const opacity = {
-  initial: { opacity: 0 },
-  animate: { opacity: 1 },
-  exit: { opacity: 0 },
-  transition: { duration: 1, delay: 0.2 },
-};
-
-export { opacity };
